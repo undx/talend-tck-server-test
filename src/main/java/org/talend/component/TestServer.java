1954d1bfa5d8c908b23d86a8a9ad3f9aa5ecf824
ypiel <ypiel@talend.com>
2020-02-25 18:41:27 +0100

    init

---
+++ src/main/java/org/talend/component/TestServer.java
@@ -0,0 +1,30 @@
+package org.talend.component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.talend.sdk.component.tools.WebServer;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Slf4j
+public class TestServer {
+
+    private final static Collection<String> serverArguments = new ArrayList<String>();
+
+    public static void main(String[] args){
+
+        int port = Integer.valueOf(System.getProperty("server.port", "8080"));
+        String gavs = System.getProperty("server.gavs", "");
+        boolean open = Boolean.valueOf(System.getProperty("server.openbrowser", "true"));
+
+        System.setProperty("talend.component.server.maven.repository", System.getProperty("repository", "/home/ypiel/.m2/repository"));
+
+        final WebServer webServer = new WebServer(serverArguments, port, log, gavs);
+        if (open) {
+            webServer.openBrowserWhenReady();
+        }
+        webServer.run();
+
+    }
+
+}
